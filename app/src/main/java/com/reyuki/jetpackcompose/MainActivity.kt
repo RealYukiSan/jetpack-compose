bdeb9218a6a61b5335d97bbf82f8e39fdaf8544f
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-23 12:56:24 +0700

    fix layout and refactor code

--- app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
@@ -5,13 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,97 +41,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun QuadrantCard(color: Color, title: String, content: String, modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .background(color)
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = content,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+val mauve = Color(0xFFB69DF8)
+val purple = Color(0xFFF6EDFF)
+
+@Composable
 fun ComposableQuadrant() {
-    val mauve = Color(0xFFB69DF8)
-    val purple = Color(0xFFF6EDFF)
-    val configuration = LocalConfiguration.current
     Column {
-        Row {
-            Box(modifier = Modifier.background(color = purple)) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(configuration.screenWidthDp.dp / 2)
-                        .height(configuration.screenHeightDp.dp / 2),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                    ) {
-                    Text(
-                        text = stringResource(R.string.text),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.text_content),
-                        textAlign = TextAlign.Justify
-                    )
-                }
-            }
-            Box(modifier = Modifier.background(color = mauve)) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(configuration.screenWidthDp.dp / 2)
-                        .height(configuration.screenHeightDp.dp / 2),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(
-                        text = stringResource(R.string.image),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.image_content),
-                        textAlign = TextAlign.Justify
-                    )
-                }
-            }
+        Row(Modifier.weight(1f)) {
+            QuadrantCard(purple, title = stringResource(R.string.text), content = stringResource(R.string.text_content), Modifier.weight(1f))
+            QuadrantCard(mauve, title = stringResource(R.string.image), content = stringResource(R.string.image_content), Modifier.weight(1f))
         }
-        Row {
-            Box(modifier = Modifier.background(color = mauve)) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(configuration.screenWidthDp.dp / 2)
-                        .height(configuration.screenHeightDp.dp / 2),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                    ) {
-                    Text(
-                        text = stringResource(R.string.row),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.row_content),
-                        textAlign = TextAlign.Justify
-                    )
-                }
-            }
-            Box(modifier = Modifier.background(color = purple)) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(configuration.screenWidthDp.dp / 2)
-                        .height(configuration.screenHeightDp.dp / 2),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                    ) {
-                    Text(
-                        text = stringResource(R.string.column),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.column_content),
-                        textAlign = TextAlign.Justify
-                    )
-                }
-            }
+        Row(Modifier.weight(1f)) {
+            QuadrantCard(mauve, title = stringResource(R.string.row), content = stringResource(R.string.row_content), Modifier.weight(1f))
+            QuadrantCard(purple, title = stringResource(R.string.column), content = stringResource(R.string.column_content), Modifier.weight(1f))
         }
     }
 }
