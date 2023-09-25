1fd475b80cce0b9b4bc1505147cbddbe8947e2ae
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-25 22:10:47 +0700

    add interactivity onboarding screen and greeting list

--- app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
@@ -3,86 +3,117 @@ package com.reyuki.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.reyuki.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ComposableQuadrant()
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun QuadrantCard(color: Color, title: String, content: String, modifier: Modifier) {
-    Column(
-        modifier = modifier
-            .background(color)
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = content,
-            textAlign = TextAlign.Justify
-        )
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
     }
-}
 
-val mauve = Color(0xFFB69DF8)
-val purple = Color(0xFFF6EDFF)
-
-@Composable
-fun ComposableQuadrant() {
-    Column {
-        Row(Modifier.weight(1f)) {
-            QuadrantCard(purple, title = stringResource(R.string.text), content = stringResource(R.string.text_content), Modifier.weight(1f))
-            QuadrantCard(mauve, title = stringResource(R.string.image), content = stringResource(R.string.image_content), Modifier.weight(1f))
-        }
-        Row(Modifier.weight(1f)) {
-            QuadrantCard(mauve, title = stringResource(R.string.row), content = stringResource(R.string.row_content), Modifier.weight(1f))
-            QuadrantCard(purple, title = stringResource(R.string.column), content = stringResource(R.string.column_content), Modifier.weight(1f))
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen { shouldShowOnboarding = false }
+        } else {
+            Greetings()
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name -> Greeting(name = name) }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier
+        .padding(vertical = 4.dp, horizontal = 8.dp)
+        .fillMaxWidth()) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(callback: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to the basics codelab!")
+        Button(modifier = Modifier.padding(vertical = 24.dp),onClick = callback) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
     JetpackComposeTheme {
-        ComposableQuadrant()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 620)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeTheme {
+        OnboardingScreen {}
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 620)
+@Composable
+fun MyAppPreview() {
+    JetpackComposeTheme {
+        MyApp()
     }
 }
