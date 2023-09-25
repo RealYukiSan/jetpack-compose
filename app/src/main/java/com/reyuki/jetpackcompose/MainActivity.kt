cbe3da1fbb5dc014f8418c371f0d7ad141394662
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-25 22:23:47 +0700

    add animation

--- app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.reyuki.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -66,7 +67,9 @@ private fun Greeting(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(targetValue = if (expanded.value) 48.dp else 0.dp,
+        label = "animate extra padding"
+    )
 
     Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier
         .padding(vertical = 4.dp, horizontal = 8.dp)
