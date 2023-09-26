92ee974f4a82136eceee3aecbbaf9d3dc3198b1f
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-26 07:17:04 +0700

    Finishing touches

--- app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/reyuki/jetpackcompose/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.reyuki.jetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,8 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,6 +32,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.reyuki.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -63,28 +72,33 @@ private fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun CardContent(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
-    val extraPadding by animateDpAsState(targetValue = if (expanded.value) 48.dp else 0.dp,
-        label = "animate extra padding"
-    )
 
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier
+    Row(modifier = Modifier.padding(24.dp).animateContentSize()) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Hello,")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold))
+            if (expanded.value) Text(text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ").repeat(4))
+        }
+        IconButton(onClick = { expanded.value = !expanded.value }) {
+            Icon(imageVector = if (expanded.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown, contentDescription = if (expanded.value) stringResource(
+                R.string.show_less
+            ) else stringResource(R.string.show_more))
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    ElevatedCard(colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary
+    ), modifier = Modifier
         .padding(vertical = 4.dp, horizontal = 8.dp)
         .fillMaxWidth()) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Hello,")
-                Text(text = name)
-            }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
-            }
-        }
+        CardContent(name)
     }
 }
 
@@ -114,6 +128,12 @@ fun OnboardingPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320, heightDp = 620)
 @Composable
 fun MyAppPreview() {
